4b2ef7216ae1a2ef778068d5e9a1f6bc24fb9487
Natalya <soltk7@yandex.ru>
2022-01-01 16:43:41 +0300

    Выполнен 1й пункт ДЗ (банковский счет)

---
+++ Homework6/BankAccount.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework6
+{
+    public enum Type
+    {
+        Дебетовый,
+        Кредитовый,
+        Накопительный
+    }
+    public class BankAccount : IEquatable<BankAccount> // наследование интерфейса для переопределения GetHashCode
+    {
+        private int numberAccount;
+        private double balanceAccount;
+        private Type typeAccount;
+
+        public int NumberAccount
+        {
+            get
+            {
+                return this.numberAccount;
+            }
+
+            set
+            {
+                this.numberAccount = value;
+            }
+
+        }
+
+        public double BalanceAccount
+        {
+            get
+            {
+                return this.balanceAccount;
+            }
+
+            set
+            {
+                this.balanceAccount = value;
+            }
+
+        }
+
+
+        public Type TypeAccount
+        {
+            get
+            {
+                return this.typeAccount;
+            }
+
+            set
+            {
+                this.typeAccount = value;
+            }
+
+        }
+
+        // переопределение оператора == для сравнения информации в двух счетах
+        public static bool operator ==(BankAccount b1, BankAccount b2)
+        {
+            return b1.NumberAccount == b2.NumberAccount && b1.TypeAccount == b2.TypeAccount;
+        }
+
+        // переопределение оператора != для сравнения информации в двух счетах
+        public static bool operator !=(BankAccount b1, BankAccount b2)
+        {
+            return b1.NumberAccount != b2.NumberAccount || b1.TypeAccount != b2.TypeAccount;
+        }
+
+        // переопределение Equals
+        public bool Equals(BankAccount other)
+        {
+            return other == this;
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as BankAccount);
+        }
+       
+
+
+        //public override bool Equals(object obj)
+        //{
+        //    if (obj == null)
+        //        return false;
+        //    BankAccount b = obj as BankAccount;
+        //    if (b as BankAccount == null)
+        //        return false;
+
+        //    return b.NumberAccount == this.NumberAccount && b.TypeAccount == this.TypeAccount;
+        //}
+
+        // переопределение ToString()
+        public override string ToString()
+        {
+            return ("Номер счета: " + numberAccount + "; Баланс счета: "
+                + balanceAccount + "; Тип счета: " + typeAccount);
+        }
+
+        // переопределение GetHashCode()
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(NumberAccount, TypeAccount);
+        }
+
+        // добавление на счет
+        public void AddToAccount(double summ)
+        {
+            this.BalanceAccount = balanceAccount + summ;
+            Console.WriteLine("Ваш счет увеличен на " + summ + ", " +
+                "общая сумма по счету теперь составляет " + this.balanceAccount);
+        }
+
+        // снятие со счета
+        public void Withdraw(double summ)
+        {
+            if (summ > this.balanceAccount)
+            {
+                Console.WriteLine("Снятие невозможно, поскольку запрошенная сумма больше остатка по счету");
+            }
+            else
+            {
+                this.BalanceAccount = balanceAccount - summ;
+                Console.WriteLine("Ваш счет уменьшен на " + summ + ", " +
+                "общая сумма по счету теперь составляет " + this.balanceAccount);
+            }
+        }
+
+  
+
+
+
+
+
+        //public void PrintAccountData() утратило актуальность, т.к. переопределен ToString()
+        //{
+        //    Console.WriteLine("Номер счета: " + numberAccount + "; Баланс счета: "
+        //        + balanceAccount + "; Тип счета: " + typeAccount);
+        //}
+    }
+}
--- Homework6/Program.cs
+++ Homework6/Program.cs
@@ -6,7 +6,47 @@ namespace Homework6
     {
         static void Main(string[] args)
         {
-           // здесь будет выполненное ДЗ
+            // простая проверка сравнения банковских счетов
+            BankAccount b1 = new BankAccount()
+            { NumberAccount = 1111,
+            BalanceAccount=10000,
+            TypeAccount=Type.Дебетовый
+            };
+            Console.WriteLine(b1.ToString());
+            Console.WriteLine($"Хэш-код: {b1.GetHashCode()}");
+            BankAccount b2 = new BankAccount()
+            {
+                NumberAccount = 1111,
+                BalanceAccount = 10000,
+                TypeAccount = Type.Дебетовый
+            };
+            Console.WriteLine(b2.ToString());
+            Console.WriteLine($"Хэш-код: {b2.GetHashCode()}");
+            BankAccount b3 = new BankAccount()
+            {
+                NumberAccount = 1111,
+                BalanceAccount = 10000,
+                TypeAccount = Type.Кредитовый
+            };
+            Console.WriteLine(b3.ToString());
+            Console.WriteLine($"Хэш-код: {b3.GetHashCode()}");
+
+            if (b1 == b2)
+                Console.WriteLine("Счета совпадают");
+            else
+                Console.WriteLine("Счета не совпадают");
+
+            if (b1 == b3)
+                Console.WriteLine("Счета совпадают");
+            else
+                Console.WriteLine("Счета не совпадают");
+
+            if(b1.Equals(b3))
+               Console.WriteLine("Счета совпадают");
+            else
+               Console.WriteLine("Счета не совпадают");
+
+
         }
     }
 }
